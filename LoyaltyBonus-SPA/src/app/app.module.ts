dba0e1a9b5b8d0a71aad25250924c5e1131a5040
Timmie <timmie.nilsson@gmail.com>
2020-09-14 10:53:13 +0200

    Added navbar, started frontend login

--- LoyaltyBonus-SPA/src/app/app.module.ts
+++ LoyaltyBonus-SPA/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ConsultantComponent } from './consultant/consultant.component';
 import { from } from 'rxjs';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ConsultantComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, ConsultantComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
