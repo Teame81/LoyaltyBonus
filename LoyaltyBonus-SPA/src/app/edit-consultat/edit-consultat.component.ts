d27bf8f77baa6212e4bd296ae50d84a09c2aa8af
Timmie <timmie.nilsson@gmail.com>
2020-10-20 10:32:11 +0200

    Fixed time zone problem in edit consultants

--- LoyaltyBonus-SPA/src/app/edit-consultat/edit-consultat.component.ts
+++ LoyaltyBonus-SPA/src/app/edit-consultat/edit-consultat.component.ts
@@ -5,6 +5,7 @@ import { Router, RouterModule } from '@angular/router';
 import { AlertifyService } from '../_services/alertify.service';
 import { EditconsultService } from '../_services/editconsult.service';
 import { PipeTransform, Pipe } from '@angular/core';
+import * as moment from 'moment'; // add this 1 of 4
 
 
 @Component({
@@ -49,6 +50,28 @@ export class EditConsultatComponent implements OnInit {
 
   editConsultant(formData: any) {
     console.log('From edit: ');
+    console.log(formData);
+    console.log("Emp DATE: ");
+    console.log(formData.employmentDate);
+    //formData.employmentDate = "2222-22-22";
+    console.log("LENGTH DATE:");
+    console.log(formData.employmentDate.toString().length);
+    if (formData.employmentDate.toString().length > 10) {
+      //let json = formData.employmentDate.toString();
+      Date.prototype.toJSON = function(){ return moment(this).format(); }
+      let json = JSON.stringify(formData.employmentDate);
+      console.log('After JSON Stringify:');
+      console.log(json);
+      json = json.substring(1);
+      console.log('after substring:' + json);
+      json = json.split('T')[0];
+      console.log('after split:' + json);
+      console.log('the date: ' + json);
+      formData.employmentDate = json;
+      console.log('After ASSING DATE FROMAT: ');
+      console.log(formData.employmentDate);
+    }
+    console.log('After From edit: ');
     console.log(formData);
     this.editConsultService.editConsultant(this.currentTextId, formData).subscribe(
       () => {
